5bf32bed4ba77050c3de7aec858f13158d7ebe1c
la_mi <la_mi@DESKTOP-0SC39IM.home>
2017-11-28 17:45:13 +0000

    Segunda Versión. KATA 4
    Implementado CIPO en java.main Kata4

--- src/kata4/main/KATA4.java
+++ src/kata4/main/KATA4.java
@@ -10,10 +10,28 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class KATA4 {
+    
+    private static final String filename = "emails.txt";
+    private static List<Mail> mailList;
+    private static Histogram<String> histogram;
+    
     public static void main(String[] args) throws IOException{
-        String filename = "emails.txt";
-        List<Mail> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        execute();
+    }
+    
+    private static void execute() throws IOException{
+        input();
+        process();
+        output();
+    }
+    
+    private static void input() throws IOException{
+        mailList = MailListReader.read(filename);
+    }
+    private static void process() throws IOException{
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    private static void output(){
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
